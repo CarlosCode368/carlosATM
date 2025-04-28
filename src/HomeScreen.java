cf923aad179004d1d318863551740745b9d69db8
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-28 11:52:09 -0700

    added payment

--- src/HomeScreen.java
+++ src/HomeScreen.java
@@ -20,6 +20,7 @@ public class HomeScreen {
 
             //All possible actions made by the user
             switch (choice) {
+                //MAKE DEPOSIT
                 case"D":
                     System.out.println("Enter the amount you want to deposit:");
                     String depositInput=scanner.nextLine().trim();
@@ -49,6 +50,38 @@ public class HomeScreen {
                         System.out.println("Deposit of $" + depositAmount + " has been added to your transactions");
                     }catch(Exception e){
                         System.out.println("An error occurred while saving your deposit.");
+                    }
+                    break;
+                   //MAKE PAYMENT
+                case "P":
+                    System.out.println("Enter the amount you want to pay:");
+                    String paymentInput=scanner.nextLine().trim();
+                    double paymentAmount=0.0;
+
+                    try {
+                        paymentAmount = Double.parseDouble(paymentInput); //So it only accepts numbers
+                    }catch(NumberFormatException e){
+                        System.out.println("System only accepts numbers.");
+                        break;
+                    }
+                    //Ask for a description of the deposit
+                    System.out.println("Enter a description for the payment (example: Family support, services, etc.):");
+                    String paymentDescription=scanner.nextLine().trim();
+
+                    try {
+                        String formattedAmount=String.format("%.2f", paymentAmount);
+
+                        //Display current date and time
+                        LocalDateTime now= LocalDateTime.now();
+                        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-MM-dd|HH:mm:ss|"); //the way you want it to be formatted and displayed
+                        String formattedDateTime= now.format(formatter);
+
+                        java.io.FileWriter writer = new java.io.FileWriter("transactionsAmount.csv", true);
+                        writer.write(formattedDateTime + paymentDescription +"|-" +formattedAmount +"\n"); //write the deposit to the file
+                        writer.close();
+                        System.out.println("Payment of $" + paymentAmount + " has been made and added to your transaction history");
+                    }catch(Exception e){
+                        System.out.println("An error occurred while saving your payment.");
                     }
                     break;
 
