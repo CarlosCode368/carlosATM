1ab224f34475e5c8a3f01271be9032475b19374a
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-28 10:32:05 -0700

    foundations

--- src/HomeScreen.java
+++ src/HomeScreen.java
@@ -1,5 +1,57 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class HomeScreen {
-    public static void main (String[] args){
-        System.out.println("Welcome to ");
+    public static void main(String[] args) {
+        // Greet the user
+        boolean isRunning = true; //a loop will start until the user inputs a correct action
+        while (isRunning) {
+            System.out.println("Welcome to The Zarkovian Bank. How can we assist you today?");
+            //Open home menu and the option for the user to input his request
+            System.out.println("D Add Deposit\nP Make Payment\nL Display the ledger screen\nX Exit application");
+            Scanner scanner = new Scanner(System.in);
+            String action = " ";
+            //Reassure the user the input requested
+            String choice = scanner.nextLine().toUpperCase(); //user can either type it in lower or upper case
+            System.out.println("You selected: " + choice);
+
+            //All possible actions made by the user
+            switch (choice) {
+                case "L":
+                    boolean inLedger=true;
+                    System.out.println("Here is a ledger of all your transactions:");
+                    try {
+                        File taskFile = new File("transactionsAmount.csv");
+                        Scanner fileScanner = new Scanner(taskFile);
+                        while (fileScanner.hasNextLine()) {
+                            String line = fileScanner.nextLine();
+                            System.out.println(line);
+                        }
+                        fileScanner.close();
+                    } catch (FileNotFoundException e) {
+                        System.out.println("No more transactions to show. Returning to Home Screen...");
+                        break;
+                    }
+                    System.out.println("All registered transactions. Type X to return to the Home Screen.");
+                    Scanner inputScanner = new Scanner(System.in);
+                    String ledgerChoice = inputScanner.nextLine().trim().toUpperCase();
+                    if (ledgerChoice.equals("x")) {
+                        inLedger = false;
+                    }
+
+                    break;
+
+                case "X":
+                    isRunning = false; //application ends
+                    System.out.println("Thank you for choosing The Zarkovian Bank");
+                    break;
+            }
+        }
     }
 }
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
-public class Main {
-}
+import java.util.ArrayList;
+class Item{
+    String name;
+    double amount;
+    Item(String name, double amount){
+        this.name=name;
+        this.amount=amount;
+    }
+        }
+        public class Main{
+    public static void main (String[] args){
+        ArrayList<Item>list=new ArrayList<>();
+        list.add(new Item("AAA",100));
+        list.add(new Item("BBB", 300));
+        double total=0;
+        for(Item item: list){
+            total +=item.amount;
+        }
+        System.out.println(total);
+    }
+        }
